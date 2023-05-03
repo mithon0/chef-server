72be4a16f1ec23683ba409f4951aec369ded714a
mithon0 <mithonhossen7244@gmail.com>
2023-05-03 13:36:43 +0600

    change recipie set name

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.get('/',(req,res)=>{
 app.get('/chef',(req,res)=>{
   res.send(chef)
 })
+app.get('/chef/:id',(req,res)=>{
+  const id =req.params.id;
+  console.log(id);
+  const selectedChef =chef.find(n=>n.id == id);
+ res.send(selectedChef);
+
+});
 
 
 
